fb13a25668ecf229c14364676f1015f902e289f5
Bakiyama <htt.ikumi@gmail.com>
2021-01-22 14:14:52 +0900

    itemモデルのバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,28 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :explain
+    validates :category_id
+    validates :condition_id
+    validates :deliver_fee_id
+    validates :area_id
+    validates :ship_id
+    validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width number."}
+  end
+
+  validates_inclusion_of :price, in:300..9999999
 end
+
+# 商品画像を1枚つけることが必須であること
+# 商品名が必須であること@
+# 商品の説明が必須であること@
+# カテゴリーの情報が必須であること@
+# 商品の状態についての情報が必須であること@
+# 配送料の負担についての情報が必須であること@
+# 発送元の地域についての情報が必須であること@
+# 発送までの日数についての情報が必須であること@
+# 価格についての情報が必須であること@
+# 価格の範囲が、¥300~¥9,999,999の間であること@
+# 販売価格は半角数字のみ保存可能であること@
