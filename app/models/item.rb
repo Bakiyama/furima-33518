9fdd3e4ced6c00f6e953fc516e75e0956b1f3907
Bakiyama <htt.ikumi@gmail.com>
2021-01-22 18:40:21 +0900

    ビューファイルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,32 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :deliver_fee
+  belongs_to :ship
+
   belongs_to :user
   has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :explain
+    validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width number."}
+  end
+
+  with_options numericality: { other_than: 1 }, presence: true  do
     validates :category_id
     validates :condition_id
     validates :deliver_fee_id
     validates :area_id
     validates :ship_id
-    validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width number."}
   end
 
   validates_inclusion_of :price, in:300..9999999
 end
 
-# 商品画像を1枚つけることが必須であること
+# 商品画像を1枚つけることが必須であること@
 # 商品名が必須であること@
 # 商品の説明が必須であること@
 # カテゴリーの情報が必須であること@
