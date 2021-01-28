a4d75b5bb5d03c4926fed20fa89a590cab3f326b
Bakiyama <htt.ikumi@gmail.com>
2021-01-28 14:25:04 +0900

    formオブジェクトのUserPurchaseに記述を追加

--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -1,10 +1,29 @@
 class UserPurchase
   include ActiveModel::Model
-  attr_accessor :
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  attr_accessor :user_id, :item_id, :postal_code, :area_id, :city, :address, :building, :telephone
 
-  # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+  end
+
+  with_options numericality: { other_than: 1 }, presence: true do
+    validates :area_id
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'is invalid. Input full-width characters.' } do
+    validates :city
+  end
+
+  validates :address, presence: true
+
+  with_options format: { with: /\A[0-9]{6,11}+\z/, message: 'is invalid. Input half-width number.' }, presence: true do
+    validates :telphone
+  end
 
   def save
-    # 各テーブルにデータを保存する処理を書く
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+
+    Recipient.create(postal_code: postal_code, area_id: area_id, city: city, address: address, building: building, telephone: telephone, purchase_id: purchase.id)
   end
 end
