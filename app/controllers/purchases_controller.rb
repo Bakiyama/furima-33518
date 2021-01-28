ce3d86390e3ca41ed1bd7334bb29eb04479ef69b
Bakiyama <htt.ikumi@gmail.com>
2021-01-28 14:18:58 +0900

    ルーティングを追加、コントローラーを修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,16 +1,19 @@
 class PurchasesController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
     @purchase = UserPurchase.new
   end
 
   def create
     @purchase = UserPurchase.new(purchase_params)
-    @purchase.save
+    if @purchase.save
+      redirect_to root_path
+    end
   end
 
   private
 
   def purchase_params
-    params.require(:user_purchase).permit()
+    params.require(:user_purchase).permit(:postal_code, :area_id, :city, :address, :building, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
