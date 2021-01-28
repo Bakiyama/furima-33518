ff637ab51571cf83ec1feecf4af6035d1500c2df
Bakiyama <htt.ikumi@gmail.com>
2021-01-28 16:31:46 +0900

    Revert "purchaseコントローラにauthenticate_userを導入"
    This reverts commit 409df2e4a13b86bc85cb3397af6e76ab05d8796f.

--- spec/factories/user_purchase.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :user_purchase do
-    postal_code     { '111-1234' }
-    area_id         { 2 }
-    city            { '札幌市' }
-    address         { '11-12' }
-    telephone        { "09098765432" }
-  end
-end
